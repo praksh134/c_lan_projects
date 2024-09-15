d49f68d23f095ac20aba5f89ca564713501ab8c2
prakslinuxusr545 <prakashyu3@gmail.com>
2024-09-15 19:31:14 +0530

    addition in c

---
+++ number.c
@@ -0,0 +1,21 @@
+/* numbers handling in c */
+
+#include <stdio.h>
+
+int main() {
+
+	int num1, num2, res;
+
+	printf("Enter the first number: ");
+	scanf("%d", &num1);
+
+	printf("Enter the second number: ");
+	scanf("%d", &num2);
+
+	res = num1 + num2;
+
+	printf("Your added answer is: %d\n", res);
+
+	return 0;
+
+}
